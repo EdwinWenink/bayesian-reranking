bacf29a4b24f66327186e527246b384d0da0efdd
EdwinWenink <edwinwenink@hotmail.com>
2020-12-21 16:11:57 +0100

    add standard error to barplots; also std in results

--- src/plot_ndcg_per_strat.py
+++ src/plot_ndcg_per_strat.py
@@ -1,25 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# @5 @10 @20
+# means @5 @10 @20
 base = (0.4741, 0.4312, 0.41)
 greedy = (0.4211, 0.3715, 0.3505)
 top5avg = (0.4086, 0.3621, 0.3431)
-#oude scores voor de geval dat (seed = 0)
-#greedy = (0.3932, 0.3592, 0.3396)
-#top5avg = (0.3835, 0.3541, 0.3367)
-#top10avg = (0.3812, 0.3495, 0.3341)  # currently not used
 
-def autolabel(bars):
-    for bar in bars:
+# standard error @5 @10 @20
+base_sem = (0.04276, 0.0381, 0.03447)
+greedy_sem = (0.043, 0.03311, 0.02896)
+top5avg_sem = (0.04291, 0.03353, 0.02873)
+
+# standard deviation @5 @10 @20
+base_std = (0.2993, 0.2667, 0.24)
+greedy_std = (0.301, 0.2318, 0.2027)
+top5avg_std = (0.3004, 0.2347, 0.2011)
+
+def autolabel(bars, yerr):
+    for i, bar in enumerate(bars):
         height = bar.get_height()
-        print(height)
-        ax.text(bar.get_x() + bar.get_width()/3., 1.05*height,
-                f'{height}',
-                rotation=90,
+        err = yerr[i]
+        ax.text(bar.get_x() + bar.get_width()/3., height+err+0.02,
+                f"$\mu$={height}",
+                #rotation=90,
                 zorder=1,
-                #ha='center',
-                va='bottom')
+                ha='left',
+                va='top')
 
 Nx = 3
 x = np.arange(Nx)
@@ -28,9 +34,9 @@ width = 0.20
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-bars1 = ax.bar(x-width, base, width, edgecolor='black', hatch='/')
-bars2 = ax.bar(x, greedy, width, edgecolor='black', hatch='.')
-bars3 = ax.bar(x+width, top5avg, width, edgecolor='black', hatch='\\')
+bars1 = ax.bar(x-width, base, width, edgecolor='black', yerr=base_sem, capsize=7, hatch='/')
+bars2 = ax.bar(x, greedy, width, edgecolor='black', yerr=greedy_sem, capsize=7, hatch='.')
+bars3 = ax.bar(x+width, top5avg, width, edgecolor='black', yerr=top5avg_sem, capsize=7, hatch='\\')
 
 ax.set_ylabel("nDCG")
 ax.set_xlabel("Rank cut-offs")
@@ -39,10 +45,11 @@ ax.set_xticks(x)
 ax.set_xticklabels( ('@5', '@10', '@20'))
 ax.legend( (bars1, bars2, bars3), ('baseline', 'greedy', 'top 5 avg'))
 
-autolabel(bars1)
-autolabel(bars2)
-autolabel(bars3)
+autolabel(bars1, base_sem)
+autolabel(bars2, greedy_sem)
+autolabel(bars3, top5avg_sem)
 
 plt.tight_layout()
+# Savefig changes the ratio of the pdf, annoyingly, so better to manually safe after plot.show()
 #plt.savefig('../results/eval/plot_ndcg_per_strat.pdf', format='pdf', bbox_inches='tight')
 plt.show()
